0788c7e85411a60dc5a968a68f774b40fcc3af51
saheeel <saheel62320@gmail.com>
2026-08-12 02:03:36 +0530

    more changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),
+        edit: resolve(import.meta.dirname, "edit.html"),
+      },
+    },
+  },
 });
